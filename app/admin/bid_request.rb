6017c28a9093933b1a4ab81c537ee235eca9c32a
Prelang Builder <builder@prelang.com>
2016-02-28 23:35:03 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'BidRequest' to allow for create, update

--- app/admin/bid_request.rb
+++ app/admin/bid_request.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register BidRequest do
+  permit_params :pick_up_street, :pick_up_city_state, :pick_up_zip, :drop_off_street, :drop_off_city_state, :drop_off_zip, :pick_up_date, :pick_up_time, :drop_off_date, :drop_off_time
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
