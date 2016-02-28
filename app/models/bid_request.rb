9795bc602357d73b3bc73468951cbe443db1dbb4
Prelang Builder <builder@prelang.com>
2016-02-28 23:34:53 +0000

    Defined validation for attribute 'drop_off_zip' on model 'BidRequest'

--- app/models/bid_request.rb
+++ app/models/bid_request.rb
@@ -2,4 +2,5 @@ class BidRequest < ActiveRecord::Base
   belongs_to :user
   has_many :horses
   validates_formatting_of :pick_up_zip, using: :us_zip
+  validates_formatting_of :drop_off_zip, using: :us_zip
 end
