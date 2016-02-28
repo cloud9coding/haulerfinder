3099606bdb47bc75d815f81f222ccdded60b1e7f
Prelang Builder <builder@prelang.com>
2016-02-28 23:34:59 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Horse' to allow for create, update

--- app/admin/horse.rb
+++ app/admin/horse.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Horse do
+  permit_params :breed, :age, :height, :weight
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
